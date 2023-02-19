7cadbe41ef0a0db9b3d6e96ed24378eb0a47a42d
Douglas Lima <douglas07a@gmail.com>
2023-02-19 15:18:13 -0300

    Atualização do codigo e melhorias nas funções JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 
-document.addEventListener('input', holderFunc)
-function holderFunc(){
+
+function input(){
+    var name = document.getElementById('texto').value;
+    document.getElementById('titular').innerHTML = name;
     
-    titular.innerHTML = texto.value
-    
+}
+
+function inputnumber(){
+    var number = document.getElementById('numero').value;
+    document.getElementById('num').innerHTML = number;
+}
+
+function inputdata(){
+    var data = document.getElementById('data').value;
+    document.getElementById('validade').innerHTML = data;
+}
+
+function inputcvc(){
+    var cvc = document.getElementById('codigo').value;
+    document.getElementById('cvc').innerHTML = cvc;
 }
